059895ec9b18fa78b402b747f602135b485eee83
David King <dave@davbo.org>
2012-04-16 17:37:38 +0100

    Added setup.py for the squirrelapi.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='squirrelapi',
+        description='Python wrapper for C3 Squirrel Voicemail HTTP API',
+        author='Mobile Oxford',
+        author_email='oucs-mobileox@maillist.ox.ac.uk',
+        url='https://github.com/oucs/telecoms-self-service',
+        version='0.1',
+        py_modules=['squirrelapi'],
+        )
