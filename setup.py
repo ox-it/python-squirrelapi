33afd51f940e005e56c5595d9a8937ebb66d6780
Martin Filliau <martin@filliau.com>
2012-06-29 15:19:35 +0100

    Added Travis CI file + moved from distutils to setuptools to support setup.py test

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 install_requires = open('requirements.txt').readlines()
+
+tests_requires = ["unittest2", "mock"]
 
 setup(name='squirrelapi',
         description='Python wrapper for C3 Squirrel Voicemail HTTP API',
@@ -9,5 +11,8 @@ setup(name='squirrelapi',
         url='https://github.com/oucs/telecoms-self-service',
         version='0.1',
         py_modules=['squirrelapi'],
+        setup_requires=["setuptools"],
         install_requires=install_requires,
+        tests_require=tests_requires,
+        test_suite = "tests",
         )
